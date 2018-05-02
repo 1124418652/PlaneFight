7ec5b7695d96ec0fb59bc0b824e5fc00343df202
hejun <1124418652@qq.com>
2018-05-02 17:25:49 +0800

    create files

---
+++ src/flying/Bullet.java
@@ -0,0 +1,5 @@
+package flying;
+
+public class Bullet {
+
+}
---
+++ src/flying/FlyingObject.java
@@ -0,0 +1,9 @@
+package flying;
+
+public abstract class FlyingObject {
+	protected int x;
+	protected int y;
+	protected int width;
+	protected int height;
+	
+}
