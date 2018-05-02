e6f3febe00b88e60211ee583582cc16c6c650cf9
hejun <1124418652@qq.com>
2018-05-02 22:07:29 +0800

    change

--- src/flying/Bullet.java
+++ src/flying/Bullet.java
@@ -1,5 +1,23 @@
 package flying;
 
-public class Bullet {
-
+public class Bullet extends FlyingObject{
+	private int speed = 3;
+	private String path;
+	
+	public Bullet(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+		this.path = 
+	}
+	
+	public boolean outofBound() 
+	{
+		return y>height;
+	}
+	
+	public void nextStep() 
+	{
+		this.y -= this.speed;
+	}
 }
--- src/flying/FlyingObject.java
+++ src/flying/FlyingObject.java
@@ -1,9 +1,67 @@
 package flying;
 
+import java.awt.*;
+import java.awt.image.*;
+
 public abstract class FlyingObject {
 	protected int x;
 	protected int y;
 	protected int width;
 	protected int height;
+	protected BufferedImage image;
 	
+	public void setX(int x)
+	{
+		this.x = x;
+	}
+	
+	public void setY(int y)
+	{
+		this.y = y;
+	}
+	
+	public void setWidth(int width)
+	{
+		this.width = width;
+	}
+	
+	public void setHeight(int height)
+	{
+		this.height = height;
+	}
+	
+	public void setImage(BufferedImage image)
+	{
+		this.image = image;
+	}
+	
+	public int getX()
+	{
+		return this.x;
+	}
+	
+	public int getY()
+	{
+		return this.y;
+	}
+	
+	public int getWidth()
+	{
+		return this.width;
+	}
+	
+	public int getHeight()
+	{
+		return this.height;
+	}
+	
+	public boolean shutbyBullet(Bullet bullet)
+	{
+		int x = bullet.x;
+		int y = bullet.y;
+		return x>=this.x && x<=(this.x+this.width) && y<=this.y && y>=(this.y+this.height);
+	}
+	
+	public abstract boolean outofBound();
+	public abstract void nextStep();
 }
